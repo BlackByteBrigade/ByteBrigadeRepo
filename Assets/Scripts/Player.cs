0cbc92a2be04dc865bed41ad71e6a7befe55483b
ItsCodeRed <elliotkeasbeyclark@gmail.com>
2023-05-20 13:58:30 -0700

    basic movement (not done)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerMovement))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static Player instance;
 
-    // Update is called once per frame
-    void Update()
+    public PlayerMovement movement;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Main Movement")]
+    public float movementSpeed;
+    public float movementMaxSpeed;
+
+    public float movementDrag;
+
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float currentSpeed = body.velocity.magnitude; // need to get speed before our changes for later use
+        body.velocity -= body.velocity.normalized * (movementDrag * Time.fixedDeltaTime);
+
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        body.velocity += input * (movementSpeed * Time.fixedDeltaTime);
+
+        if (body.velocity.magnitude > movementMaxSpeed)
+        {
+            body.velocity = body.velocity.normalized * currentSpeed; // this is the later use -- makes sure we dont accelerate further if we are above max speed
+        }
+    }
+}
