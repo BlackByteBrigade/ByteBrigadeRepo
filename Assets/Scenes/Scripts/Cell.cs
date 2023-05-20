4b0332530ca72b87eeae5697825bcb1ec5472852
ItsCodeRed <elliotkeasbeyclark@gmail.com>
2023-05-20 13:20:13 -0700

    added cell base class

---
+++ Assets/Scenes/Scripts/Cell.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public int health;
+
+    /// <summary>
+    /// Cell takes specified damage
+    /// </summary>
+    /// <param name="damage"></param>
+    /// <returns>Whether or not the enemy died</returns>
+    public virtual bool TakeDamage(int damage)
+    {
+        health = Mathf.Max(health - damage, 0); // so that health never goes below zero (for player healthbar to not go below zero)
+
+        bool isDead = health <= 0;
+        if (isDead)
+        {
+            Die();
+        }
+
+        return isDead;
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
