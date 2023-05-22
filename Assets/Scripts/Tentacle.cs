b8dc01a4d71e565218aab0b5ef1b9ca85ef5b006
ItsCodeRed <elliotkeasbeyclark@gmail.com>
2023-05-22 11:08:57 -0700

    finished first version

--- Assets/Scripts/Tentacle.cs
+++ Assets/Scripts/Tentacle.cs
@@ -6,21 +6,31 @@ public class Tentacle : MonoBehaviour
 {
     public int length;
     public LineRenderer lineRenderer;
-    public Vector3[] linePositions;
+
+    public float segmentLength;
+    public float smoothSpeed;
+    public float drag;
+
+    private Vector3[] segmentPoses;
+    private Vector3[] segmentVels;
 
     private void Start()
     {
         lineRenderer.positionCount = length;
-        linePositions = new Vector3[length];
+        segmentPoses = new Vector3[length];
+        segmentVels = new Vector3[length];
     }
 
     private void Update()
     {
-        linePositions[0] = transform.position;
+        segmentPoses[0] = transform.position;
 
         for (int i = 1; i < length; i++)
         {
-
+            Vector3 targetDir = (transform.right + (segmentPoses[i] - segmentPoses[i - 1]).normalized * drag).normalized;
+            segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir * segmentLength, ref segmentVels[i], smoothSpeed);
         }
+
+        lineRenderer.SetPositions(segmentPoses);
     }
 }
