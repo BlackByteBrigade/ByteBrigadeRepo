99a2a6ed4300796a2a02672c7983e332cd44c162
ItsCodeRed <elliotkeasbeyclark@gmail.com>
2023-05-23 00:15:06 -0700

    added to basic enemy

--- Assets/Scripts/EnemyBasic.cs
+++ Assets/Scripts/EnemyBasic.cs
@@ -6,34 +6,49 @@ public class EnemyBasic : Enemy
 {
     [SerializeField] private float moveDistance;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float driftSpeed;
+    [SerializeField] private float targetRadius;
     [SerializeField] private Animator anim;
 
     [SerializeField] private Transform[] path;
 
+    private Rigidbody2D body;
+    private Vector2 targetPos;
     private int pathIndex = 0;
 
     private void Start()
     {
         base.Start();
+        body = GetComponent<Rigidbody2D>();
+        TargetNextPoint();
         StartCoroutine(Move());
     }
 
     private IEnumerator Move()
     {
         Vector2 currentPos = transform.position;
-        Vector2 targetPos = path[pathIndex].position;
-        transform.position += (Vector3)(targetPos - currentPos).normalized * moveDistance;
-        transform.up = -(targetPos - currentPos).normalized;
+        Vector2 prevTargetPos = path[(pathIndex - 1 < 0 ? path.Length : pathIndex) - 1].position;
+        Vector2 targetDir = (targetPos - currentPos).normalized;
+
+        body.velocity = targetDir * driftSpeed;
+        transform.position += (Vector3)targetDir * moveDistance;
+        transform.up = -targetDir;
 
         anim.Play("move", -1, 0f);
 
         yield return new WaitForSeconds(moveSpeed);
 
-        if (Vector2.Distance(transform.position, path[pathIndex].position) < moveDistance)
+        if (Vector2.Dot(targetPos - (Vector2)transform.position, targetPos - prevTargetPos) < 0)
         {
-            pathIndex = (pathIndex + 1) % path.Length;
+            TargetNextPoint();
         }
 
         StartCoroutine(Move());
+    }
+
+    private void TargetNextPoint()
+    {
+        pathIndex = (pathIndex + 1) % path.Length;
+        targetPos = (Vector2)path[pathIndex].position + Random.insideUnitCircle * targetRadius;
     }
 }
