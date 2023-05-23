34edc870c71d988462084be0b5a9645ac6ffad89
ItsCodeRed <elliotkeasbeyclark@gmail.com>
2023-05-22 18:40:02 -0700

    added tentacle wiggle

--- Assets/Scripts/Tentacle.cs
+++ Assets/Scripts/Tentacle.cs
@@ -7,12 +7,18 @@ public class Tentacle : MonoBehaviour
     public int length;
     public LineRenderer lineRenderer;
 
+    [Header("Tentacle Feel")]
     public float segmentLength;
     public float smoothSpeed;
     public float drag;
 
+    [Header("Tentacle Wiggle")]
+    public float wiggleMagnitude;
+    public float wiggleSpeed;
+
     private Vector3[] segmentPoses;
     private Vector3[] segmentVels;
+    private float startAngle;
 
     private void Start()
     {
@@ -21,6 +27,7 @@ public class Tentacle : MonoBehaviour
         lineRenderer.positionCount = length;
         segmentPoses = new Vector3[length];
         segmentVels = new Vector3[length];
+        startAngle = Random.Range(0, 360);
 
         ResetLine();
     }
@@ -31,7 +38,7 @@ public class Tentacle : MonoBehaviour
 
         for (int i = 1; i < length; i++)
         {
-            Vector3 targetDir = (transform.right + (segmentPoses[i] - segmentPoses[i - 1]).normalized * drag).normalized;
+            Vector3 targetDir = Quaternion.Euler(0, 0, Mathf.Sin(startAngle + Time.time * wiggleSpeed - (wiggleSpeed * i / length)) * wiggleMagnitude) * (transform.right + (segmentPoses[i] - segmentPoses[i - 1]).normalized * (drag * Mathf.Max(segmentVels[i].magnitude - wiggleMagnitude / 10, 0))).normalized;
             segmentPoses[i] = Vector3.SmoothDamp(segmentPoses[i], segmentPoses[i - 1] + targetDir * segmentLength, ref segmentVels[i], smoothSpeed);
         }
 
