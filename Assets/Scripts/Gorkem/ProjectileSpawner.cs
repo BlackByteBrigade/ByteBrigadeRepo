4d64757e2123611828a70270a7ea9b81a0b22f80
gorkemkoksal <81961582+gorkemkoksal@users.noreply.github.com>
2023-05-26 22:03:03 +0300

    1

---
+++ Assets/Scripts/Gorkem/ProjectileSpawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+   // [SerializeField] private GameObject projectile;
+   // private ObjectPool<GameObject> pool;
+   // private void Awake()
+   // {
+   //     pool = new ObjectPool<GameObject>(OnCreateObject, OnGet, OnRelease, OnDestroy, false, 10, 15);
+   // }
+   //private GameObject OnCreateObject()
+   // {
+   //     var instance=Instantiate(projectile,Vector3.zero,Quaternion.identity);
+   //     instance.gameObject.SetActive(false);
+   //     return instance;
+   // }
+   // private void OnGet()
+   // {
+
+   // }
+}
