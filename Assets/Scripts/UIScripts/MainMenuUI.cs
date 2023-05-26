79aba24e365256ffb478745a0f2b53fb7f513238
brow88 <davidbrown.88@hotmail.co.uk>
2023-05-26 22:46:38 +0100

    Main Menu Layout

--- Assets/Scripts/UIScripts/MainMenuUI.cs
+++ Assets/Scripts/UIScripts/MainMenuUI.cs
@@ -8,25 +8,27 @@ public class MainMenuUI : MonoBehaviour
 {
     [SerializeField] private Button playButton;
     [SerializeField] private Button settingsButton;
-    [SerializeField] private Button quitButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button creditButton;
 
     private void Awake() {
         playButton.onClick.AddListener(() => {
             //Play game
             Debug.Log("Play Game");
-
         });
         settingsButton.onClick.AddListener(() => {
             //game settings
             Debug.Log("settings");
-
         });
-        quitButton.onClick.AddListener(() => {
+        exitButton.onClick.AddListener(() => {
             //Exit Game
             Application.Quit();
             Debug.Log("Quit Game");
         });
-
+        creditButton.onClick.AddListener(() => {
+            //Game credits
+            Debug.Log("Game credits");
+        });
     }
 
 }
