40590d045c12305da358ad4cb69f2036fb4d60f5
brow88 <davidbrown.88@hotmail.co.uk>
2023-05-28 16:00:20 +0100

    menu Update

--- Assets/Scripts/UIScripts/MainMenu/MainMenuUI.cs
+++ Assets/Scripts/UIScripts/MainMenu/MainMenuUI.cs
@@ -10,24 +10,48 @@ public class MainMenuUI : MonoBehaviour
 
     [SerializeField] private Button playButton;
     [SerializeField] private Button exitButton;
+
+    private Animator animator;
     
     //For Settings and credit buttons go to the MainMenuController script
     private void Awake() {
+        
+        //get animator
+        animator = GetComponent<Animator>();
+
         //Play game
         playButton.onClick.AddListener(() => {
             //Play Animation of exit buttons
-            
-            //Play button loads game scene
-            SceneManager.LoadScene(sceneToLoadWhenPlayIsClicked);
+            animator.SetBool("Menu", false);
+            //Play button loads game scene after timer
+            startGame = true;
         });
         
         //Exit Game
-        exitButton.onClick.AddListener(() => {
-            
+        exitButton.onClick.AddListener(() => {   
             Application.Quit();
         });
-        
-        //Game credits
     }
+
+    //timer variables
+    private bool startTimer = false;
+    private bool startGame = false;
+    private float timer = 0;
+    private float timerMax = 1;     
+    
+    private void Update() {     
+        if(startGame == true){
+            timer += Time.deltaTime;
+            if(timer >= timerMax){
+                SceneManager.LoadScene(sceneToLoadWhenPlayIsClicked);
+            }
+        }
+    }
+
+
+
+
+
+
 
 }
